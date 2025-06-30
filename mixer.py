26e27e8c8c24e375cbcf6aac8a386cca2761b490
VbossStudios <vbossstudios@gmail.com>
2025-06-30 16:07:15 -0500

    Delete mixer.py

--- mixer.py
+++
@@ -1,8 +0,0 @@
-from pydub import AudioSegment
-
-def mix_audio(vocal_path, instr_path, output_path):
-    vocals = AudioSegment.from_file(vocal_path)
-    instr = AudioSegment.from_file(instr_path)
-    mixed = instr.overlay(vocals)
-    mixed.export(output_path, format="wav")
-    return output_path
